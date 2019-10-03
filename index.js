abdd3101a6afce91e38328fe76e75a1150f12563
spiano <lafleurpiano@gmail.com>
2019-10-03 09:26:54 -0500

    set up files and models

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-app.listen(3000, () => console.log('app is running'))
+const express = require('express');
+const app = express();
+const noteController = require('');
+const userController = require('');
+const parser = require('body-parser');
+
+app.use(parser.urlencoded({ extended:true }));
+app.use("/", noteController);
+app.get("/", (req, res) => {
+    res.redirect("/");
+});
+
+
+app.listen(3000, () => console.log('app is running on port 3000'));
 
 // DO NOT REMOVE THIS LINE:
-module.exports = app
+module.exports = app;
